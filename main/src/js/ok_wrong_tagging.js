4fbd939af80a7182495d62ce10cc89cfc6e2022d
Rudolf Großmann <rg58@gmx.de>
2022-04-29 17:32:18 +0200

    separate OkWrong tagging

---
+++ main/src/js/ok_wrong_tagging.js
@@ -0,0 +1,26 @@
+"use strict";
+import $ from "jquery";
+import {
+    isH5P
+} from "./dom.js";
+
+export default function setOkWrongTag(key, isEqual, instance) {
+    if (isH5P()) {
+        H5P.setScoredResult(1,5,instance,false, false);
+    } else {
+        var truefalse = $(key)[0];
+        if (isEqual) {
+            $(truefalse).css({
+                "color": "green",
+                "font-size": "30pt"
+            });
+            truefalse.innerHTML = "&nbsp;&#x2714;";
+        } else {
+            $(truefalse).css({
+                "color": "red",
+                "font-size": "30pt"
+            });
+            truefalse.innerHTML = "&nbsp;&#x21AF;";
+        }
+    }
+}
