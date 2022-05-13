5fac49295d428d0eaa6b8f69c9b2e4d9ec6beb94
Rudolf Großmann <rg58@gmx.de>
2022-05-13 17:36:27 +0200

    compose HTML for MathQuill using params/H5Pbridge.

--- h5p/development/H5PEditor.FormulaAppletEditor-0.13/scripts/formulaapplet-editor.js
+++ h5p/development/H5PEditor.FormulaAppletEditor-0.13/scripts/formulaapplet-editor.js
@@ -1,3 +1,5 @@
+"use strict"
+
 /**
  * ExpressionSelector widget module
  *
@@ -5,7 +7,7 @@
  */
 
 var H5P = H5P || {};
-console.log('Here is new formulaapplet-editor.js 0.13.' + H5Pbridge.config.patchversion);
+console.log('Here is stub of formulaapplet-editor.js 0.13.' + H5Pbridge.config.patchversion);
 
 H5PEditor.widgets.formulaAppletEditor = H5PEditor.FormulaAppletEditor = (function ($) {
 
@@ -18,6 +20,10 @@ H5PEditor.widgets.formulaAppletEditor = H5PEditor.FormulaAppletEditor = (functio
    * @param {Object} params
    * @param {function} setValue
    */
+
+  // available H5Pbridge methods
+  // console.log(H5Pbridge);
+
   function FormulaAppletEditor(parent, field, params, setValue) {
     this.parent = parent;
     this.field = field;
@@ -34,76 +40,103 @@ H5PEditor.widgets.formulaAppletEditor = H5PEditor.FormulaAppletEditor = (functio
    */
   FormulaAppletEditor.prototype.appendTo = function ($wrapper) {
     var self = this;
-    const id = ns.getNextFieldId(this.field);
-    // TODO change class-name
-    var inner = '<input type="text" id="' + id + '" class="h5p-formulaapplet-expression-picker">';
-    var html = H5PEditor.createFieldMarkup(this.field, inner, id);
-    self.$item = H5PEditor.$(html);
-    self.$formulaApplet = self.$item.find('.h5p-formulaapplet-expression-picker');
+    const  h5p_id = ns.getNextFieldId(this.field);
+    // console.log(self);
+    var params = self.parent.params;
+    // compose an HTML tag to be used by MathQuill using params and H5Pbridge
+    if (typeof params.id === 'undefined') {
+      // TODO get random id
+      params.id = 'new_id';
+    }
+    var html = '<p class="formula_applet" id="' + params.id + '-edit"';
+    var solution = '';
+    if (params.formulaAppletMode === 'manu') {
+      try {
+        solution = H5Pbridge.decode(params.data_b64);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+
+    var expression = params.TEX_expression;
+    if (typeof expression === 'undefined') {
+      expression = '17 + {{result}} = 21';
+    }
+    html += '>';
+    var language = H5Pbridge.docLang();
+    var temp = H5Pbridge.H5P_to_MathQuill(expression, solution, language, true); //isEditor=true
+    // temp: like LATEX, but special syntax for MathQuill added
+    // wrap temp into <span> and close <p class="formula_applet"...> tag
+    html += '<span id="math-field">' + temp + '</span><\p>';
+    console.log('Assembled html: ' + html);
+
+    var inner = '<input type="text" id="' + id + '" class="formula_applet">dummy';
+    // TODO next line: replace inner by html
+    var fieldMarkup = H5PEditor.createFieldMarkup(this.field, inner,  h5p_id);
+    self.$item = H5PEditor.$(fieldMarkup);
+    self.$formulaApplet = self.$item.find('.formula_applet');
 
     self.config = {
       appendTo: self.$item[0],
       preferredFormat: 'hex',
-      expression: self.getExpression(),
+      // expression: self.getExpression(),
+      expression: " get: myExpression",
       change: function (expression) {
-        self.setExpression(expression);
+        // self.setExpression(expression);
+        console.log('do, what is necessary if expression is changed ' + expression);
       },
       hide: function (expression) {
         // Need this to get expression if cancel is clicked
-        self.setExpression(expression);
+        // self.setExpression(expression);
+        console.log('do, what is necessary if expression is hidden ' + expression);
       }
     };
 
     // DELETE parts concerning spectrum
-    // // Make it possible to set spectrum config
+    // Make it possible to set spectrum config
     // if (self.field.spectrum !== undefined) {
     //   self.config = $.extend(self.config, self.field.spectrum);
     // }
 
-    // // Create expression picker widget
-    // self.$expressionPicker.spectrum(self.config);
+    // // Create color picker widget
+    // self.$colorPicker.spectrum(self.config);
 
     self.$item.appendTo($wrapper);
   };
 
   /**
-   * Return expressioncode in "css" format
+   * Return colorcode in "css" format
    *
-   * @method expressionToString
-   * @param  {Object}      expression
+   * @method colorToString
+   * @param  {Object}      color
    * @return {String}
    */
-  ExpressionSelector.prototype.expressionToString = function (expression) {
-    // DELETE parts concerning spectrum
+  FormulaAppletEditor.prototype.colorToString = function (color) {
     // switch (this.config.preferredFormat) {
-    //   case 'rgb':
-    //     return expression.toRgbString();
-    //   case 'hsv':
-    //     return expression.toHsvString();
-    //   case 'hsl':
-    //     return expression.toHslString();
-    //   default:
-    //     return expression.toHexString();
+    //   case 'rgb': return color.toRgbString();
+    //   case 'hsv': return color.toHsvString();
+    //   case 'hsl': return color.toHslString();
+    //   default: return color.toHexString();
     // }
-    return 'ExpressionString (test)';
+    return 'test - string returned by colorToString(color)';
   };
 
   /**
-   * Hide expression selector
+   * Hide color selector
    * @method hide
    */
-  ExpressionSelector.prototype.hide = function () {
+  FormulaAppletEditor.prototype.hide = function () {
     // DELETE parts concerning spectrum
-    // this.$expressionPicker.spectrum('hide');
+    // this.$colorPicker.spectrum('hide');
   };
   /**
-   * Save the expression
+   * Save the color
    *
-   * @param {Object} expression The
+   * @param {Object} color The
    */
-  ExpressionSelector.prototype.setExpression = function (expression) {
+  FormulaAppletEditor.prototype.setColor = function (color) {
     // Save the value, allow null
-    this.params = (expression === null ? null : this.expressionToString(expression));
+    this.params = (color === null ? null : this.colorToString(color));
     this.setValue(this.field, this.params);
 
     this.changes.forEach(function (cb) {
@@ -111,7 +144,7 @@ H5PEditor.widgets.formulaAppletEditor = H5PEditor.FormulaAppletEditor = (functio
     })
   };
 
-  ExpressionSelector.prototype.getExpression = function () {
+  FormulaAppletEditor.prototype.getColor = function () {
     var isEmpty = (this.params === null || this.params === "");
     return isEmpty ? null : this.params;
   };
@@ -119,12 +152,12 @@ H5PEditor.widgets.formulaAppletEditor = H5PEditor.FormulaAppletEditor = (functio
   /**
    * Validate the current values.
    */
-  ExpressionSelector.prototype.validate = function () {
+  FormulaAppletEditor.prototype.validate = function () {
     this.hide();
     return (this.params !== undefined && this.params.length !== 0);
   };
 
-  ExpressionSelector.prototype.remove = function () {};
+  FormulaAppletEditor.prototype.remove = function () {};
 
-  return ExpressionSelector;
+  return FormulaAppletEditor;
 })(H5P.jQuery);
