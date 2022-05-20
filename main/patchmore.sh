af0f3173ac4734677a29537b869ea46eaeb75920
Rudolf Großmann <rg58@gmx.de>
2022-05-20 17:14:50 +0200

    suppress CRLF warnings of 'git add' (patchmore.sh)

--- main/patchmore.sh
+++ main/patchmore.sh
@@ -11,14 +11,19 @@ echo "[patchmore.sh] version number: $VERSIONNUMBER"
 rm ../version-*.txt
 # "git add" also has to "add" the deletion!
 git add ../version-*.txt
+
+# create new versionnumber file
 touch "../version-$VERSIONNUMBER.txt"
+#'git add' causes staging!
 git add ../version-$VERSIONNUMBER.txt
 
 
-FILENAME=src/js/test_lineendings.txt
-rm $FILENAME
-cp $FILENAME.ori $FILENAME
-echo "[patchmore.sh] copied file $FILENAME.ori to $FILENAME"
+#FILENAME=./test_lineendings.txt
+#rm $FILENAME
+#cp $FILENAME.ori $FILENAME
+#sed -i "s/__VERSION__/$VERSIONNUMBER/g" $FILENAME
+#git add $FILENAME  2>warning.txt
+#echo "[patchmore.sh] patched file $FILENAME with version number $VERSIONNUMBER"
 
 FILENAME=src/js/config.json
 rm $FILENAME
@@ -27,7 +32,7 @@ sed -i "s/__MAJORVERSION__/$MAJORNUMBER/g" $FILENAME
 sed -i "s/__MINORVERSION__/$MINORNUMBER/g" $FILENAME
 sed -i "s/__PATCHVERSION__/$PATCHNUMBER/g" $FILENAME
 sed -i "s/__VERSION__/$VERSIONNUMBER/g" $FILENAME
-git add $FILENAME
+git add $FILENAME  2>warning.txt
 echo "[patchmore.sh] patched file $FILENAME with version number $VERSIONNUMBER"
 
 FILENAME=./package.json
@@ -36,7 +41,7 @@ cp $FILENAME.ori $FILENAME
 sed -i "s/__MAJORVERSION__/$MAJORNUMBER/g" $FILENAME
 sed -i "s/__MINORVERSION__/$MINORNUMBER/g" $FILENAME
 sed -i "s/__PATCHVERSION__/$PATCHNUMBER/g" $FILENAME
-git add $FILENAME
+git add $FILENAME  2>warning.txt
 echo "[patchmore.sh] patched file $FILENAME with version number $VERSIONNUMBER"
 sleep 4
 
@@ -46,7 +51,7 @@ cp $FILENAME.ori $FILENAME
 sed -i "s/__MAJORVERSION__/$MAJORNUMBER/g" $FILENAME
 sed -i "s/__MINORVERSION__/$MINORNUMBER/g" $FILENAME
 sed -i "s/__PATCHVERSION__/$PATCHNUMBER/g" $FILENAME
-git add $FILENAME
+git add $FILENAME  2>warning.txt
 echo "[patchmore.sh] patched file $FILENAME with version number $VERSIONNUMBER"
 
 FILENAME=../h5p/development/H5PEditor.FormulaAppletEditor-$MAJORNUMBER.$MINORNUMBER/library.json
@@ -55,12 +60,17 @@ cp $FILENAME.ori $FILENAME
 sed -i "s/__MAJORVERSION__/$MAJORNUMBER/g" $FILENAME
 sed -i "s/__MINORVERSION__/$MINORNUMBER/g" $FILENAME
 sed -i "s/__PATCHVERSION__/$PATCHNUMBER/g" $FILENAME
-git add $FILENAME
+git add $FILENAME  2>warning.txt
 echo "[patchmore.sh] patched file $FILENAME with version number $VERSIONNUMBER"
 
 ((PATCHNUMBER=PATCHNUMBER+1))
 echo $PATCHNUMBER > ./nextpatchnumber.txt
 # echo "[patch.sh] next patch number: $PATCHNUMBER"
+
+rm ./warning.txt
+# "git add" also has to "add" the deletion, but only if stashed before!
+# git add ./warning.txt
+
 # https://linuxconfig.org/bash-script-pause-script-before-proceeding
 read -p "Pausing for 60 seconds" -t 60
 
