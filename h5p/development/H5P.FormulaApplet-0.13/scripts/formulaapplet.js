01085029a3a32fc2e83b96f647eecb2546324278
Rudolf Großmann <rg58@gmx.de>
2022-05-28 17:03:39 +0200

    adjust position of afterAppend(), no fa_obj needed

--- h5p/development/H5P.FormulaApplet-0.13/scripts/formulaapplet.js
+++ h5p/development/H5P.FormulaApplet-0.13/scripts/formulaapplet.js
@@ -17,188 +17,6 @@ H5P.FormulaApplet = (function ($, Question) {
   var STATE_FINISHED = 'finished';
 
 
-
-  var counter;
-  // afterAppend is called multiple times, once for every appended FormulaApplet
-  function afterAppend(fa_obj) {
-    // if counter is undefined, init with value 0.
-    // if counter is defined (increased), do not change
-    counter = counter || 0;
-    if (counter === 0) {
-      // things to be done once
-      console.log('formulaapplet.js 0.13.' + H5Pbridge.config.patchversion);
-      // mathQuillify legacy applets with syntax <p class="formula_applet solution">...</p>
-      mathQuillifyLegacyApplets();
-      H5Pbridge.initVirtualKeyboardnoEditor();
-    }
-    counter++;
-
-    var options = fa_obj.options;
-    var id = options.id;
-    // mathQuillifying
-    var MQ = H5Pbridge.MQ;
-    console.log('try to mathquillify ' + id);
-    var $el = $('#' + id + '.formula_applet:not(.mq-math-mode)');
-    if (typeof $el === 'undefined') {
-      throw id + ' not found';
-    }
-
-    var waitForDomElem = H5Pbridge.createWaiter();
-    waitForDomElem.name = 'waiter for domElem ';
-    waitForDomElem.max_count = 100; // 100*200ms=100*0,2s= 20s
-    waitForDomElem.condition = function () {
-      $el = $('#' + id + '.formula_applet:not(.mq-math-mode)');
-      return (typeof $el[0] !== 'undefined');
-    };
-    waitForDomElem.doTheRest = async function () {
-      var domElem = $('#' + id + '.formula_applet:not(.mq-math-mode)')[0];
-      // console.log(domElem);
-      var expression = domElem.innerHTML;
-      console.log('Mathquillify ' + id + ': ' + expression);
-      // look for {{resultfield}}:
-      var hasResultField = (expression.indexOf('{{result}}') >= 0);
-      // hasResultField will be used in clickEvent
-      // remember here, because expression will be changed soon by H5P_to_MathQuill
-      if (hasResultField) {
-        $(domElem).addClass('hasresultfield');
-      }
-      var hasSolution = (options.formulaAppletMode === 'manu');
-
-      var language = H5Pbridge.docLang();
-      //start with empty user input
-      var solution = '';
-      var isEditor = false;
-      var temp = H5Pbridge.H5P_to_MathQuill(expression, solution, language, isEditor);
-      domElem.innerHTML = temp;
-
-      var mqEditableField;
-      try {
-        mqEditableField = MQ.StaticMath(domElem);
-        // MQ.StaticMath seems to generate a mqEditableField
-      } catch (err) {
-        console.error('Error using MQ.StaticMath: ' + err);
-        console.trace();
-      }
-
-      $el.on('click', clickHandler);
-
-      function clickHandler(ev) {
-        try {
-          if (typeof domElem !== 'undefined') {
-            if (hasResultField) {
-              ev.stopPropagation(); // avoid body click
-              // deselect all applets
-              $(".formula_applet").removeClass('selected');
-              $(".formula_applet").off('virtualKeyboardEvent');
-              $(domElem).addClass('selected');
-              // attach virtualkeyboardEvent
-              $("button.keyb_button").removeClass('selected');
-              if (($('#virtualKeyboard').css('display') || 'none') === 'none') {
-                // if virtual keyboard is hidden, show keyboard button
-                $(domElem).nextAll("button.keyb_button:first").addClass('selected');
-              }
-            } else {
-              // element has no ResultField (static formula)
-              console.log(ev.currentTarget.id + ' has no ResultField');
-            }
-          }
-        } catch (error) {
-          //TODO ERROR ReferenceError: ev is not defined (maybe if hasSolution = false) 
-          console.log('ERROR ' + error);
-        }
-      }
-
-      // attach button for evoking virtual keyboard
-      try {
-        // make virtual keyboard show/hide by mouseclick
-        ($('<button class="keyb_button">\u2328</button>')).insertAfter($el);
-        $('button.keyb_button').on('mousedown', function () {
-          H5Pbridge.showVirtualKeyboard();
-          $("button.keyb_button").removeClass('selected');
-        });
-        // insert span for right/wrong tag
-        // will be replaced by H5P scoring
-        $('<span class="truefalse">&nbsp;</span>').insertAfter($el);
-      } catch (error) {
-        console.error(error);
-      }
-      if ($('#' + id).hasClass('mq-math-mode')) {
-        console.log(id + ': SUCCESS');
-      }
-
-      //create MathField and attach edithandler
-      if (hasResultField) {
-        mqEditableField = $el.find('.mq-editable-field')[0];
-        // console.log(mqEditableField);
-        var mf = MQ.MathField(mqEditableField, {});
-        // console.log(mf);
-        mf.config({
-          handlers: {
-            edit: () => {
-              mqEditableField.focus();
-              mathQuillEditHandler();
-            },
-            //TODO ENTER: is case enter necessary?
-            enter: () => {
-              mathQuillEditHandler();
-            },
-          }
-        });
-      }
-
-      //TODO no need for argument "options" because defined in surrounding function
-      function mathQuillEditHandler() {
-        if (H5Pbridge.isEditHandlerActive()) {
-          var data_b64 = options.data_b64;
-          var unitAuto = options.formulaAppletPhysics;
-          var precision = options.sanitizedPrecision;
-          var definitionSets = options.definitionSets;
-
-          if (unitAuto) {
-            // has to be done before checkIfEqual
-            H5Pbridge.makeAutoUnitstring(mf);
-          }
-
-          var isEqual;
-          if (hasSolution) {
-            isEqual = H5Pbridge.checkIfEqual(mf.latex(), data_b64, definitionSets, precision);
-          } else {
-            // look at whole equation: input field and surroundings
-            var mfContainer = MQ.StaticMath(domElem);
-            isEqual = H5Pbridge.checkIfEquality(mfContainer.latex(), definitionSets, precision);
-            console.log(mfContainer.latex() + ' isEqual= ' + isEqual);
-          }
-          // see ok_wrong_tagging.js
-          var key = '#' + id + '.formula_applet + span.truefalse';
-          H5Pbridge.setOkWrongTag(key, isEqual);
-        }
-      }
-
-
-    };
-    waitForDomElem.start();
-
-    async function mathQuillifyLegacyApplets() {
-      await H5Pbridge.domLoad;
-      var MQ = H5Pbridge.MQ;
-      console.log(MQ);
-      console.log('mathQuillifyLegacyApplets');
-      $('p.formula_applet.solution').each(function (index) {
-        var domElem = $(this)[0];
-        var expression = domElem.innerHTML;
-        var solution = '';
-        var language = H5Pbridge.docLang();
-        var isEditor = false;
-        var temp = H5Pbridge.H5P_to_MathQuill(expression, solution, language, isEditor);
-        domElem.innerHTML = temp;
-        console.log(index, temp);
-        MQ.StaticMath(domElem);
-      });
-    }
-  }
-
-
-
   //TODO resize
   // $(document).trigger('resize');
 
@@ -320,9 +138,10 @@ H5P.FormulaApplet = (function ($, Question) {
     self.setContent(self.createFormulaApplet(), {
       'class': self.params.behaviour.separateLines ? 'h5p-separate-lines' : ''
     });
-    afterappend(self);
+    self.afterAppend();
+
     // ... and buttons
-    self.registerButtons(); //TODO button logic
+    self.registerButtons();
   };
   // ```
 
@@ -498,6 +317,192 @@ H5P.FormulaApplet = (function ($, Question) {
       prec = "0.000001";
     }
     return prec;
+  }
+
+
+  var counter;
+  // afterAppend is called multiple times, once for every appended FormulaApplet
+  // function afterAppend(fa_obj) {
+  C.prototype.afterAppend = function () {
+    var self=this;
+    // if counter is undefined, init with value 0.
+    // if counter is defined (increased), do not change
+    counter = counter || 0;
+    if (counter === 0) {
+      // things to be done once
+      console.log('formulaapplet.js 0.13.' + H5Pbridge.config.patchversion);
+      // mathQuillify legacy applets with syntax <p class="formula_applet solution">...</p>
+      mathQuillifyLegacyApplets();
+      H5Pbridge.initVirtualKeyboardnoEditor();
+    }
+    counter++;
+
+    //TODO DELETE 
+    // console.log(self);
+    // console.log(fa_obj);
+
+    var options = self.options;
+    var id = options.id;
+    // mathQuillifying
+    var MQ = H5Pbridge.MQ;
+    console.log('try to mathquillify ' + id);
+    var $el = $('#' + id + '.formula_applet:not(.mq-math-mode)');
+    if (typeof $el === 'undefined') {
+      throw id + ' not found';
+    }
+
+    var waitForDomElem = H5Pbridge.createWaiter();
+    waitForDomElem.name = 'waiter for domElem ';
+    waitForDomElem.max_count = 100; // 100*200ms=100*0,2s= 20s
+    waitForDomElem.condition = function () {
+      $el = $('#' + id + '.formula_applet:not(.mq-math-mode)');
+      return (typeof $el[0] !== 'undefined');
+    };
+    waitForDomElem.doTheRest = async function () {
+      var domElem = $('#' + id + '.formula_applet:not(.mq-math-mode)')[0];
+      // console.log(domElem);
+      var expression = domElem.innerHTML;
+      console.log('Mathquillify ' + id + ': ' + expression);
+      // look for {{resultfield}}:
+      var hasResultField = (expression.indexOf('{{result}}') >= 0);
+      // hasResultField will be used in clickEvent
+      // remember here, because expression will be changed soon by H5P_to_MathQuill
+      if (hasResultField) {
+        $(domElem).addClass('hasresultfield');
+      }
+      var hasSolution = (options.formulaAppletMode === 'manu');
+
+      var language = H5Pbridge.docLang();
+      //start with empty user input
+      var solution = '';
+      var isEditor = false;
+      var temp = H5Pbridge.H5P_to_MathQuill(expression, solution, language, isEditor);
+      domElem.innerHTML = temp;
+
+      var mqEditableField;
+      try {
+        mqEditableField = MQ.StaticMath(domElem);
+        // MQ.StaticMath seems to generate a mqEditableField
+      } catch (err) {
+        console.error('Error using MQ.StaticMath: ' + err);
+        console.trace();
+      }
+
+      $el.on('click', clickHandler);
+
+      function clickHandler(ev) {
+        try {
+          if (typeof domElem !== 'undefined') {
+            if (hasResultField) {
+              ev.stopPropagation(); // avoid body click
+              // deselect all applets
+              $(".formula_applet").removeClass('selected');
+              $(".formula_applet").off('virtualKeyboardEvent');
+              $(domElem).addClass('selected');
+              // attach virtualkeyboardEvent
+              $("button.keyb_button").removeClass('selected');
+              if (($('#virtualKeyboard').css('display') || 'none') === 'none') {
+                // if virtual keyboard is hidden, show keyboard button
+                $(domElem).nextAll("button.keyb_button:first").addClass('selected');
+              }
+            } else {
+              // element has no ResultField (static formula)
+              console.log(ev.currentTarget.id + ' has no ResultField');
+            }
+          }
+        } catch (error) {
+          //TODO ERROR ReferenceError: ev is not defined (maybe if hasSolution = false) 
+          console.log('ERROR ' + error);
+        }
+      }
+
+      // attach button for evoking virtual keyboard
+      try {
+        // make virtual keyboard show/hide by mouseclick
+        ($('<button class="keyb_button">\u2328</button>')).insertAfter($el);
+        $('button.keyb_button').on('mousedown', function () {
+          H5Pbridge.showVirtualKeyboard();
+          $("button.keyb_button").removeClass('selected');
+        });
+        // insert span for right/wrong tag
+        // will be replaced by H5P scoring
+        $('<span class="truefalse">&nbsp;</span>').insertAfter($el);
+      } catch (error) {
+        console.error(error);
+      }
+      if ($('#' + id).hasClass('mq-math-mode')) {
+        console.log(id + ': SUCCESS');
+      }
+
+      //create MathField and attach edithandler
+      if (hasResultField) {
+        mqEditableField = $el.find('.mq-editable-field')[0];
+        // console.log(mqEditableField);
+        var mf = MQ.MathField(mqEditableField, {});
+        // console.log(mf);
+        mf.config({
+          handlers: {
+            edit: () => {
+              mqEditableField.focus();
+              mathQuillEditHandler();
+            },
+            //TODO ENTER: is case enter necessary?
+            enter: () => {
+              mathQuillEditHandler();
+            },
+          }
+        });
+      }
+
+      //TODO no need for argument "options" because defined in surrounding function
+      function mathQuillEditHandler() {
+        if (H5Pbridge.isEditHandlerActive()) {
+          var data_b64 = options.data_b64;
+          var unitAuto = options.formulaAppletPhysics;
+          var precision = options.sanitizedPrecision;
+          var definitionSets = options.definitionSets;
+
+          if (unitAuto) {
+            // has to be done before checkIfEqual
+            H5Pbridge.makeAutoUnitstring(mf);
+          }
+
+          var isEqual;
+          if (hasSolution) {
+            isEqual = H5Pbridge.checkIfEqual(mf.latex(), data_b64, definitionSets, precision);
+          } else {
+            // look at whole equation: input field and surroundings
+            var mfContainer = MQ.StaticMath(domElem);
+            isEqual = H5Pbridge.checkIfEquality(mfContainer.latex(), definitionSets, precision);
+            console.log(mfContainer.latex() + ' isEqual= ' + isEqual);
+          }
+          // see ok_wrong_tagging.js
+          var key = '#' + id + '.formula_applet + span.truefalse';
+          H5Pbridge.setOkWrongTag(key, isEqual);
+        }
+      }
+
+
+    };
+    waitForDomElem.start();
+
+    async function mathQuillifyLegacyApplets() {
+      await H5Pbridge.domLoad;
+      var MQ = H5Pbridge.MQ;
+      console.log(MQ);
+      console.log('mathQuillifyLegacyApplets');
+      $('p.formula_applet.solution').each(function (index) {
+        var domElem = $(this)[0];
+        var expression = domElem.innerHTML;
+        var solution = '';
+        var language = H5Pbridge.docLang();
+        var isEditor = false;
+        var temp = H5Pbridge.H5P_to_MathQuill(expression, solution, language, isEditor);
+        domElem.innerHTML = temp;
+        console.log(index, temp);
+        MQ.StaticMath(domElem);
+      });
+    }
   }
 
   return C;
